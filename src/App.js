bb7b690c045f3f2c55b3e523ff0822cf2f7f2e28
oscarmouras28 <oscar.mourasp@gmail.com>
2022-03-08 20:29:18 -0300

    Funcional

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import injectContext from "./store/pololitoContext";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SignUp from "./views/SignUp";
+import LandingPage from "./views/LandingPage";
 
 const App = () => {
     return <>
         <Router>
             <Routes>
+                <Route path="/" element={<LandingPage/>} />
                 <Route path="/SignUp" element={<SignUp/>} />
             </Routes>
         </Router>
