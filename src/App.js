5682bd2ff929e98263f4af293a6596270eb6ae48
oscarmouras28 <oscar.mourasp@gmail.com>
2022-03-08 16:40:12 -0300

    Ya visible para la web

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import injectContext from "./store/pololitoContext";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SignUp from "./views/SignUp";
 
 const App = () => {
-  return <>
-    <Router>
-      <Navbar />
-      <Routes >
-        < Route/>
-        < Route/>
-        < Route/>
-        < Route/>
-        < Route/>
-      </Routes>
-    </ Router>
-  </>
+    return <>
+        <Router>
+            <Routes>
+                <Route path="/SignUp" element={<SignUp/>} />
+            </Routes>
+        </Router>
+    </>
 }
 
-export default injectContext;
+export default App;
--- src/components/SignUp.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const SignUp = () => {
-    return <div> ola </div> ;
-}
-
-export default SignUp;
