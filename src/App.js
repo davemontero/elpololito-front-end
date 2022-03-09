e95341c9f7533ca8d7f31cf06a5da27b1d850822
matias <matias.banderas@ug,uchile.cl>
2022-03-09 20:55:28 -0300

    Avances LandingPage Navbar provisorio

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import injectContext from "./store/pololitoContext";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./components/Navbar"
 import SignUp from "./views/SignUp";
 import LandingPage from "./views/LandingPage";
 
 const App = () => {
     return <>
         <Router>
+        <Navbar />
             <Routes>
-                <Route path="/" element={<LandingPage/>} />
-                <Route path="/SignUp" element={<SignUp/>} />
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/SignUp" element={<SignUp />} />
             </Routes>
         </Router>
     </>
