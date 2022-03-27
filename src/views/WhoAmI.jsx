e36ee5faee28e3eba05d785ca7ffc188f9ead31b
matias <matias.banderas@ug,uchile.cl>
2022-03-26 22:39:10 -0300

    PORFIN TENEMOS JWT FULL FUNCIONAL PAPA

--- src/views/WhoAmI.jsx
+++ src/views/WhoAmI.jsx
@@ -7,12 +7,13 @@ const WhoAmI = () => {
     fetch("http://localhost:5000/who_am_i", {
       method: 'get',
       headers: {
-          'Content-Type': 'application/json',
-          "Authorization": "Bearer ${localStorage.getItem('jwt')}",
+          
+        Authorization: `Bearer ${localStorage.getItem('jwt')}`,
       },
     })
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => console.log(data))
+  ;
 
   console.log(localStorage);
   });
